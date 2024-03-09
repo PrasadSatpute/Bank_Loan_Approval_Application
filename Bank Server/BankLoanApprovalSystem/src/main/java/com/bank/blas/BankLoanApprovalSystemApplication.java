664ee1c6370b48778f74d031b51298f3f9518a80
PRASAD PURUSHOTTAM SATPUTE <32232749+PrasadSatpute@users.noreply.github.com>
2024-03-09 14:52:01 +0530

    Initial BLAS Project

---
+++ Bank Server/BankLoanApprovalSystem/src/main/java/com/bank/blas/BankLoanApprovalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bank.blas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLoanApprovalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankLoanApprovalSystemApplication.class, args);
+	}
+
+}
---
+++ Bank Server/BankLoanApprovalSystem/src/test/java/com/bank/blas/BankLoanApprovalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.blas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLoanApprovalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
