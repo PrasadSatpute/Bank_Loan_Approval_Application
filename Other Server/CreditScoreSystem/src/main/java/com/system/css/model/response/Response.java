f421dba7a0ae9b807fe77c9ecdc28f689cee4ddb
PRASAD PURUSHOTTAM SATPUTE <32232749+PrasadSatpute@users.noreply.github.com>
2024-03-29 11:57:16 +0530

    Update Response.java

--- Other Server/CreditScoreSystem/src/main/java/com/system/css/model/response/Response.java
+++ Other Server/CreditScoreSystem/src/main/java/com/system/css/model/response/Response.java
@@ -1,6 +1,8 @@
 package com.system.css.model.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -14,6 +16,28 @@ public class Response {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int customerID;
 
+
+    @Column(columnDefinition = "TEXT")
     @JsonProperty("customer")
-    private Customer customer;
+    private String customer;
+
+    // Getter and setter for customer field
+    public Customer getCustomer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(customer, Customer.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+            return null;
+        }
+    }
+
+    public void setCustomer(Customer customer) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            this.customer = objectMapper.writeValueAsString(customer);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+    }
 }
